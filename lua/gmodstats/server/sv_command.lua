1434f6cdc7d64f0e00643ce2c304f14c47324147
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-08-10 00:06:15 -0400

    Update sv_command.lua

--- lua/gmodstats/server/sv_command.lua
+++ lua/gmodstats/server/sv_command.lua
@@ -3,7 +3,6 @@ local function toN(t,n) return tonumber(string.sub(t,n)) end
 hook.Add("PlayerSay", "ShowStats", function(ply, text)
 	if string.StartWith(text, "!stats admin") then
 		if ply:IsAdmin() or ply:IsSuperAdmin() then
-<<<<<<< Updated upstream
 			local stats = {}
 			for _, v in ipairs(player.GetAll()) do
 				local query = sql.Query("SELECT * FROM stats_mp WHERE player = " .. v:SteamID64())
@@ -11,7 +10,6 @@ hook.Add("PlayerSay", "ShowStats", function(ply, text)
 					table.insert(stats, query[1])
 				end
 			end
-=======
 			local v = 0
 			
 			if toN(text, -1) then
@@ -29,16 +27,10 @@ hook.Add("PlayerSay", "ShowStats", function(ply, text)
 
 			print("Bytes: " .. bytes)
 
->>>>>>> Stashed changes
 			net.Start("gstatsAdmin")
 				net.WriteUInt(bytes, 16)
 				net.WriteData(stats, bytes)
 			net.Send(ply)
-<<<<<<< Updated upstream
-
-			return ""
-=======
->>>>>>> Stashed changes
 		end
 	elseif (string.lower(text) == "!stats") then
 		local stats = sql.Query("SELECT * FROM stats_mp WHERE player = " .. sql.SQLStr(ply:SteamID64()) .. ";")
